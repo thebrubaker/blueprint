d77142735fa3cd69a144849d19f432ec9e3e1f7c
Joel Brubaker <pk.joel@gmail.com>
2017-04-08 11:32:23 -0700

    - two tests passing for making a blueprint

--- test/blueprint_test.js
+++ test/blueprint_test.js
@@ -1,9 +1,10 @@
-// import CoreBlueprint from '../lib/core'
-// import Blueprint from '../lib/blueprint'
-// import { name, driver, location, embeds, hasMany } from '../lib/symbols'
+import CoreBlueprint from '../lib/core'
+import Blueprint from '../lib/blueprint'
+import { name, driverType, location, embeds, hasMany } from '../lib/symbols'
 import assert from 'assert'
 
 describe('Blueprint', () => {
+
   it('is configured with options describing the blueprint', () => {
     const config = {
       name: 'name',
@@ -12,14 +13,25 @@ describe('Blueprint', () => {
       embeds: 'embeds',
       hasMany: 'hasMany'
     }
-    assert.equal(config, config)
-    // const blueprint = new Blueprint(config)
-    // assert.equal(blueprint instanceof Blueprint, true)
-    // assert.equal(blueprint instanceof CoreBlueprint, true)
-    // assert.equal(blueprint[name], 'name')
-    // assert.equal(blueprint[driver], 'driver')
-    // assert.equal(blueprint[location], 'location')
-    // assert.equal(blueprint[embeds], 'embeds')
-    // assert.equal(blueprint[hasMany], 'hasMany')
+    const blueprint = new Blueprint(config)
+    assert.equal(blueprint instanceof Blueprint, true)
+    assert.equal(blueprint instanceof CoreBlueprint, true)
+    assert.equal(blueprint[name], 'name')
+    assert.equal(blueprint[driverType], 'driver')
+    assert.equal(blueprint[location], 'location')
+    assert.equal(blueprint[embeds], 'embeds')
+    assert.equal(blueprint[hasMany], 'hasMany')
+  })
+
+  it('is has methods for fetching, creating, updating and deleting models', () => {
+    const config = {
+      name: 'name',
+      driver: 'driver'
+    }
+    const blueprint = new Blueprint(config)
+    assert.equal(typeof blueprint.fetch === 'function', true)
+    assert.equal(typeof blueprint.create === 'function', true)
+    assert.equal(typeof blueprint.update === 'function', true)
+    assert.equal(typeof blueprint.delete === 'function', true)
   })
 })
