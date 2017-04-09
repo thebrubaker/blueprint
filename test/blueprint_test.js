062f7e856b04c6fc20af501c06485a5098d1b4f6
Joel Brubaker <pk.joel@gmail.com>
2017-04-09 08:22:15 -0700

    - removed old index test - added three more blueprint tests, passing

--- test/blueprint_test.js
+++ test/blueprint_test.js
@@ -1,37 +1,71 @@
 import CoreBlueprint from '../lib/core'
 import Blueprint from '../lib/blueprint'
-import { name, driverType, location, embeds, hasMany } from '../lib/symbols'
+import { name, driverType, location, embeds, hasMany, drivers, driver, config } from '../lib/symbols'
 import assert from 'assert'
+import sinon from 'sinon'
 
 describe('Blueprint', () => {
 
   it('is configured with options describing the blueprint', () => {
-    const config = {
+    const options = {
       name: 'name',
       driver: 'driver',
       location: 'location',
       embeds: 'embeds',
       hasMany: 'hasMany'
     }
-    const blueprint = new Blueprint(config)
-    assert.equal(blueprint instanceof Blueprint, true)
-    assert.equal(blueprint instanceof CoreBlueprint, true)
+    const blueprint = new Blueprint(options)
+    assert(blueprint instanceof Blueprint)
+    assert(blueprint instanceof CoreBlueprint)
     assert.equal(blueprint[name], 'name')
     assert.equal(blueprint[driverType], 'driver')
     assert.equal(blueprint[location], 'location')
     assert.equal(blueprint[embeds], 'embeds')
     assert.equal(blueprint[hasMany], 'hasMany')
+    assert.equal(blueprint[config], options)
   })
 
-  it('is has methods for fetching, creating, updating and deleting models', () => {
-    const config = {
+  it('has methods for fetching, creating, updating and deleting models', () => {
+    const options = {
       name: 'name',
       driver: 'driver'
     }
-    const blueprint = new Blueprint(config)
-    assert.equal(typeof blueprint.fetch === 'function', true)
-    assert.equal(typeof blueprint.create === 'function', true)
-    assert.equal(typeof blueprint.update === 'function', true)
-    assert.equal(typeof blueprint.delete === 'function', true)
+    const blueprint = new Blueprint(options)
+    assert(typeof blueprint.fetch === 'function')
+    assert(typeof blueprint.create === 'function')
+    assert(typeof blueprint.update === 'function')
+    assert(typeof blueprint.delete === 'function')
+  })
+
+  it('loads a set of drivers', () => {
+    const options = {
+      name: 'name',
+      driver: 'foo'
+    }
+    const driverSet = {
+      foo: sinon.spy()
+    }
+    const blueprint = new Blueprint(options)
+    blueprint.loadDrivers(driverSet)
+    assert.equal(blueprint[drivers], driverSet)
+  })
+
+  it('sets a driver from the available drivers, and throws an exception if its invalid', () => {
+    const options = {
+      name: 'name',
+      driver: 'foo'
+    }
+    const driverSet = {
+      foo: sinon.spy()
+    }
+    const blueprint = new Blueprint(options)
+    // Throws if no drivers have been loaded
+    assert.throws(() => blueprint.setDriver('foo'))
+    blueprint.loadDrivers(driverSet)
+    blueprint.setDriver('foo')
+    assert.equal(blueprint[driverType], 'foo')
+    assert.equal(blueprint[driver], driverSet.foo)
+    // Throws if an invalid driver is set
+    assert.throws(() => blueprint.setDriver('bar'))
   })
 })
